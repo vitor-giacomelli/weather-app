9fcab22fafa5b0645ee475fae794ec15e8a719a3
Vitor Giacomelli <kruxer1985@hotmail.com>
2019-08-05 12:20:34 -0300

    Remove debuggers from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,77 +8,71 @@ import { CidadeEntityComponent } from './shared/cidade-entity/cidade-entity.comp
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
   weather;
   cidades;
-public orlw2: any;
-  constructor(private weatherService: WeatherService, private apiBackendService : BackendApiService){
+  public orlw2: any;
+  constructor(private weatherService: WeatherService, private apiBackendService: BackendApiService) {
 
   }
-  ngOnInit(){
-   
+  ngOnInit() {
+
   }
   getCidades() {
-  
-  this.apiBackendService.getCidades()
-  .subscribe(
-    res  =>{      
-      console.log(res);
-      this.cidades = res;
-      debugger;
-    },
-    err => {
-      debugger;
-      console.log(err);
-    },
-    () =>{
-      debugger;
-    })
-}
 
-postCidade(nomeCidade: HTMLElement, pais: HTMLElement, codigoCidade: HTMLElement){
-this.getCidades();
-var x = +codigoCidade.innerHTML;
-var cidade = new CidadeEntityComponent(nomeCidade.innerHTML, pais.innerHTML, x);
-this.apiBackendService.postCidade(cidade)
-            .subscribe(res  =>{              
-              console.log(res);
-              debugger;
-            },
-            err =>{
-              console.log(err);
-              debugger;
-            });
-debugger;
-}
-  getWeather(cityName: string, countryCode: string){
+    this.apiBackendService.getCidades()
+      .subscribe(
+        res => {
+          console.log(res);
+          this.cidades = res;
+        },
+        err => {
+          console.log(err);
+        })
+  }
+
+  postCidade(nomeCidade: HTMLElement, pais: HTMLElement, codigoCidade: HTMLElement) {
+    this.getCidades();
+    var x = +codigoCidade.innerHTML;
+    var cidade = new CidadeEntityComponent(nomeCidade.innerHTML, pais.innerHTML, x);
+    this.apiBackendService.postCidade(cidade)
+      .subscribe(res => {
+        console.log(res);
+      },
+        err => {
+          console.log(err);
+
+        });
+
+  }
+  getWeather(cityName: string, countryCode: string) {
     this.weatherService.getWeather(cityName, countryCode)
       .subscribe(
         res => {
           console.log(res);
-          this.weather = res},
+          this.weather = res
+        },
         err => {
           console.log(err);
           if (err.status == "404")
             alert('Cidade não encontrada');
         }
-      ) 
+      )
   }
 
-  submitLocation(cityName: HTMLInputElement, countryCode: HTMLInputElement){
-    if (cityName.value && countryCode.value){
+  submitLocation(cityName: HTMLInputElement, countryCode: HTMLInputElement) {
+    if (cityName.value && countryCode.value) {
       this.getWeather(cityName.value, countryCode.value);
       cityName.value = '';
       countryCode.value = '';
-    }else{
+    } else {
       alert('Favor preencher todos os campos');
     }
-
 
     cityName.focus();
     return false;
   }
 
- 
+
 }
